6ba67e2619b8cbf98363779bf06f059397f41f86
shihab20105 <shihabur10@gmail.com>
2025-08-03 21:00:24 +0100

    Discovered What is Event, different types of event in web

---
+++ 26-1 What is Event, different types of ev/JS/events.js
@@ -0,0 +1,5 @@
+/*                          26-1 What is Event, different types of event in web */
+console.log('This is separate JS file');
+
+
+/* Basically if you want to you can put 'onclick' on any DOM element  */
